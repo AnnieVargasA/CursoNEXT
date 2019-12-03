ec0f4457076004ba33004a37d6ae87790a6348c7
Annie Vargas <annie.vargas94@gmail.com>
2019-12-03 17:21:32 -0600

    Styled JSX

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,30 @@ export default class extends React.Component{
     return <div>
       <h1>Hola Annie!</h1>
       <p>Este es el curso de Next.js con React.js</p>
+      <img src="../static/platzi-logo.png" alt="Platzi-logo"/>
+      <style jsx>{`
+        h1{
+          color: red;
+        }
+        :global(div p){
+          color: green;
+        }
+        img{
+          max-width: 50%;
+          display: block;
+          margi: 0 auto;
+        }
+        div{
+          width:100%
+        }
+      `}
+      </style>
+      <style jsx global>{`
+        body{
+          background: white;
+        }
+      `}
+      </style>
     </div>
-
   }
 }
